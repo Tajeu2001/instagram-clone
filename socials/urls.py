4ec1874dfe96c45a195f43f62378f1e8310b4521
Tajeu2001 <tajeusanta@gmail.com>
2021-07-14 00:23:08 +0300

    Update installed apps

--- socials/urls.py
+++ socials/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url,include
+from django.urls import path,include
 from django.contrib.auth import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'admin/', admin.site.urls),
-    url(r'' ,include('instagram.urls')),
+    path(r'admin/', admin.site.urls),
+    path(r'' ,include('instagram.urls')),
     
 ]    
     
